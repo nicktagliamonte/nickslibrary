81155e5bc42faac0980366094fbd6dc2ee98030d
nicktagliamonte <ntagliamonte28@gmail.com>
2025-03-01 11:51:48 -0500

    BTree

--- src/main/java/com/nickslibrary/Main.java
+++ src/main/java/com/nickslibrary/Main.java
@@ -1,16 +1,6 @@
 package com.nickslibrary;
 
-import com.nickslibrary.datastructures.hashing.HashTable;
-import com.nickslibrary.datastructures.hashing.HashTable.CollisionResolution;
-
-/**
- *
- */
 public class Main {
     public static void main(String[] args) {
-        HashTable<String, Integer> smallTable = new HashTable<>(2, CollisionResolution.OPEN_ADDRESSING);
-        smallTable.put("key1", 100);
-        smallTable.put("key2", 200);
-        smallTable.put("key3", 300); // Should trigger rehash
     }
 }
--- src/main/java/com/nickslibrary/utils/tree/BTreeNode.java
+++ src/main/java/com/nickslibrary/utils/tree/BTreeNode.java
@@ -1,169 +1,97 @@
 package com.nickslibrary.utils.tree;
 
-public class BTreeNode<T extends Comparable<T>> extends TreeNode<T> {
-    private int degree;
-    private T[] keys;
-    private BTreeNode<T>[] children;
-    private int numKeys;
-    private boolean isLeaf;
+public class BTreeNode<T extends Comparable<T>> {
+    public int[] keys; 
+    int t; 
+    public BTreeNode<T>[] children;
+    public int n; 
+    boolean leaf; 
 
     @SuppressWarnings("unchecked")
-    public BTreeNode(int degree, boolean isLeaf, T value) {
-        super(value);
-        this.degree = degree;
-        this.isLeaf = isLeaf;
-        this.keys = (T[]) new Comparable[2 * degree - 1];
-        this.children = new BTreeNode[2 * degree];
-        this.numKeys = 0;
+    public BTreeNode(int t, boolean leaf) {
+        this.t = t;
+        this.leaf = leaf;
+
+        keys = new int[2 * t - 1];
+        children = new BTreeNode[2 * t];
+        n = 0;
     }
 
-    public T getKey(int index) {
-        return keys[index];
-    }
-
-    public void setDegree(int degree) {
-        this.degree = degree;
-    }
-
-    public T[] getKeys() {
-        return keys;
-    }
-
-    public void setKeys(T[] keys) {
-        this.keys = keys;
-    }
-
-    public BTreeNode<T>[] getChildren() {
-        return children;
-    }
-
-    public void setChildren(BTreeNode<T>[] children) {
-        this.children = children;
-    }
-
-    public void setKey(int index, T value) {
-        keys[index] = value;
-    }
-
-    public BTreeNode<T> getChild(int index) {
-        return children[index];
-    }
-
-    public void setChild(int index, BTreeNode<T> child) {
-        children[index] = child;
-    }
-
-    public int getNumKeys() {
-        return numKeys;
-    }
-
-    public void setNumKeys(int numKeys) {
-        this.numKeys = numKeys;
-    }
-
-    public boolean isLeaf() {
-        return isLeaf;
-    }
-
-    public void setLeaf(boolean isLeaf) {
-        this.isLeaf = isLeaf;
-    }
-
-    public int getDegree() {
-        return degree;
-    }
-
-    public int findKeyIndex(T key) {
+    public BTreeNode<T> search(int key) {
         int i = 0;
-        while (i < numKeys && key.compareTo(keys[i]) > 0) {
+
+        while (i < n && key > keys[i])
             i++;
-        }
-        return i;
+
+        if (keys[i] == key)
+            return this;
+
+        if (leaf)
+            return null;
+
+        return children[i].search(key);
     }
 
-    public void insertNonFull(T key) {
-        int i = numKeys - 1;
+    public void insertNonFull(int key) {
+        int i = n - 1;
 
-        if (isLeaf) {
-            while (i >= 0 && key.compareTo(keys[i]) < 0) {
+        if (leaf) {
+            while (i >= 0 && key < keys[i]) {
                 keys[i + 1] = keys[i];
                 i--;
             }
             keys[i + 1] = key;
-            numKeys++;
+            n++;
         } else {
-            while (i >= 0 && key.compareTo(keys[i]) < 0) {
+            while (i >= 0 && key < keys[i])
                 i--;
-            }
             i++;
 
-            if (children[i].numKeys == (2 * degree - 1)) {
-                splitChild(i);
-                if (key.compareTo(keys[i]) > 0) {
+            if (children[i].n == 2 * t - 1) {
+                splitChild(i, children[i]);
+                if (key > keys[i])
                     i++;
-                }
             }
             children[i].insertNonFull(key);
         }
     }
 
-    public void splitChild(int index) {
-        BTreeNode<T> fullChild = children[index];
-        BTreeNode<T> newChild = new BTreeNode<>(degree, fullChild.isLeaf, fullChild.keys[degree]);
+    public void splitChild(int i, BTreeNode<T> y) {
+        BTreeNode<T> z = new BTreeNode<T>(y.t, y.leaf);
+        z.n = t - 1;
 
-        newChild.numKeys = degree - 1;
+        for (int j = 0; j < t - 1; j++)
+            z.keys[j] = y.keys[j + t];
 
-        for (int j = 0; j < degree - 1; j++) {
-            newChild.keys[j] = fullChild.keys[j + degree];
-        }
-        if (!fullChild.isLeaf) {
-            for (int j = 0; j < degree; j++) {
-                newChild.children[j] = fullChild.children[j + degree];
-            }
+        if (!y.leaf) {
+            for (int j = 0; j < t; j++)
+                z.children[j] = y.children[j + t];
         }
 
-        for (int j = numKeys; j > index; j--) {
+        y.n = t - 1;
+
+        for (int j = n; j >= i + 1; j--)
             children[j + 1] = children[j];
-        }
-        children[index + 1] = newChild;
 
-        for (int j = numKeys - 1; j >= index; j--) {
+        children[i + 1] = z;
+
+        for (int j = n - 1; j >= i; j--)
             keys[j + 1] = keys[j];
-        }
-        keys[index] = fullChild.keys[degree - 1];
 
-        fullChild.numKeys = degree - 1;
-        numKeys++;
+        keys[i] = y.keys[t - 1];
+        n++;
     }
 
-    public boolean searchKey(T key) {
-        int i = findKeyIndex(key);
-        if (i < numKeys && keys[i].compareTo(key) == 0) {
-            return true;
-        }
-        return !isLeaf && children[i].searchKey(key);
-    }
+    public void printInOrder() {
+        int i;
 
-    public T getPredecessor(int index) {
-        BTreeNode<T> current = children[index];
-        while (!current.isLeaf) {
-            current = current.children[current.numKeys];
+        for (i = 0; i < n; i++) {
+            if (!leaf)
+                children[i].printInOrder();
+            System.out.print(keys[i] + " ");
         }
-        return current.keys[current.numKeys - 1];
-    }
 
-    public T getSuccessor(int index) {
-        BTreeNode<T> current = children[index + 1];
-        while (!current.isLeaf) {
-            current = current.children[0];
-        }
-        return current.keys[0];
-    }
-
-    public void removeKey(int index) {
-        for (int i = index; i < numKeys - 1; i++) {
-            keys[i] = keys[i + 1];
-        }
-        numKeys--;
+        if (!leaf)
+            children[i].printInOrder();
     }
 }
